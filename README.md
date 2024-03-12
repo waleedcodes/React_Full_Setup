a4de4ea8fb8ec6b1e55cf53addfcfb6bf695df91
WaleedCodes <110061477+waleedcodes@users.noreply.github.com>
2024-03-12 13:35:09 +0500

    some changes

---
+++ README.md
@@ -0,0 +1,1 @@
+# React setup Project
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App;
